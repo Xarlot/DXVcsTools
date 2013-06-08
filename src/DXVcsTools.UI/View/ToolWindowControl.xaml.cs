c182b95d39bbec1275af9ce7b40a0d254773d825
Xarlot <maximatorr@bk.ru>
2013-06-08 13:21:21 +0400

    refactoring

---
+++ src/DXVcsTools.UI/View/ToolWindowControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DXVcsTools.UI.View {
+    /// <summary>
+    /// Interaction logic for ToolWindowControl.xaml
+    /// </summary>
+    public partial class ToolWindowControl : UserControl {
+        public ToolWindowControl() {
+            InitializeComponent();
+        }
+    }
+}
--- src/DXVcsTools.Version/VersionInfo.cs
+++ src/DXVcsTools.Version/VersionInfo.cs
@@ -1,7 +1,7 @@
 ﻿namespace DXVcsTools.Version {
     public sealed class VersionInfo {
-        public const string Major = "1";
-        public const string Minor = "2";
+        public const string Major = "2";
+        public const string Minor = "0";
         public const string Build = "1";
         public const string Revision = "0";
 
