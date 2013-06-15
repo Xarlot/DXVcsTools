3f23396d985f3ee2330721b5b46eb5b2510790c1
Xarlot <maximatorr@bk.ru>
2013-06-15 14:09:33 +0400

    refactoring

--- src/DXVcsTools.UI/ViewModel/OptionsViewModel.cs
+++ src/DXVcsTools.UI/ViewModel/OptionsViewModel.cs
@@ -10,7 +10,7 @@ using System.Xml;
 using System.Xml.Serialization;
 using DXVcsTools.Core;
 
-namespace DXVcsTools.Core {
+namespace DXVcsTools.UI {
     public class OptionsViewModel {
         public OptionsViewModel() {
             Branches = new List<DXVcsBranch>();
