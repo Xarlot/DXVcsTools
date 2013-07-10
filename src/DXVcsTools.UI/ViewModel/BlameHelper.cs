f02b5c2ab348d33d4b1790c8df5262ce0308834f
Xarlot <maximatorr@bk.ru>
2013-07-10 18:34:21 +0400

    add vs2013

---
+++ src/DXVcsTools.UI/ViewModel/BlameHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using DevExpress.Xpf.Core;
+using DXVcsTools.DXVcsClient;
+using DXVcsTools.UI;
+
+namespace DXVcsTools.Core {
+    public class BlameHelper {
+    }
+}
