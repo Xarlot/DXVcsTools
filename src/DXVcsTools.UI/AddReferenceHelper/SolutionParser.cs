e40179c7f47213ac99f50fd12433674ba55c03d8
Xarlot <maximatorr@bk.ru>
2013-07-31 19:33:03 +0400

    msbuild.parse

--- src/DXVcsTools.UI/AddReferenceHelper/SolutionParser.cs
+++ src/DXVcsTools.UI/AddReferenceHelper/SolutionParser.cs
@@ -13,10 +13,15 @@ namespace DXVcsTools.UI.AddReferenceHelper {
         public void Parse() {
             solutionWrapper = typeof(BuildManager).Assembly.CreateInstance("Microsoft.Build.Construction.SolutionParser", true, BindingFlags.NonPublic | BindingFlags.Instance, null , new object[] { }, CultureInfo.CurrentCulture, null);
             SetSolutionPath(solutionWrapper, solutionPath);
+            DoParse(solutionWrapper);
         }
         void SetSolutionPath(object solution, string path) {
             var propertyInfo = solution.GetType().GetProperty("SolutionFile", BindingFlags.Instance | BindingFlags.NonPublic);
             propertyInfo.SetValue(solution, path);
+        }
+        void DoParse(object solution) {
+            var methodInfo = solution.GetType().GetMethod("ParseSolutionFile", BindingFlags.Instance | BindingFlags.NonPublic);
+            methodInfo.Invoke(solution, null);
         }
     }
 }
