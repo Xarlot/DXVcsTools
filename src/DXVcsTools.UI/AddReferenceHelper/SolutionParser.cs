85ae2165aba0769f498584856cc649739db90173
Xarlot <maximatorr@bk.ru>
2013-08-01 20:05:27 +0400

    refactoring

--- src/DXVcsTools.UI/AddReferenceHelper/SolutionParser.cs
+++ src/DXVcsTools.UI/AddReferenceHelper/SolutionParser.cs
@@ -10,16 +10,21 @@ using Microsoft.Build.Execution;
 
 namespace DXVcsTools.UI.AddReferenceHelper {
     public class SolutionParser {
-        string solutionPath;
-        object solutionWrapper;
+        readonly string solutionPath;
         public SolutionParser(string path) {
             solutionPath = path;
         }
         public IEnumerable<string> Parse() {
-            solutionWrapper = typeof(BuildManager).Assembly.CreateInstance("Microsoft.Build.Construction.SolutionParser", true, BindingFlags.NonPublic | BindingFlags.Instance, null , new object[] { }, CultureInfo.CurrentCulture, null);
+            object solutionWrapper = GetSolutionWrapper();
             SetSolutionPath(solutionWrapper, solutionPath);
             DoParse(solutionWrapper);
-            foreach (object projectItem in (IEnumerable)solutionWrapper.GetType().GetProperty("ProjectsInOrder", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(solutionWrapper)) {
+            return GetAssembliesForAddReference(solutionWrapper);
+        }
+        IEnumerable<string> GetAssembliesForAddReference(object solutionWrapper) {
+            var items = (IEnumerable)solutionWrapper.GetType().GetProperty("ProjectsInOrder", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(solutionWrapper);
+            if (items == null || !items.Cast<object>().Any())
+                yield break;
+            foreach (object projectItem in items) {
                 ProjectRootElement root = ProjectRootElement.Open(GetAbsolutePath(projectItem));
                 foreach (var item in GetDXReferences(root))
                     yield return item;
@@ -39,6 +44,9 @@ namespace DXVcsTools.UI.AddReferenceHelper {
         void DoParse(object solution) {
             var methodInfo = solution.GetType().GetMethod("ParseSolutionFile", BindingFlags.Instance | BindingFlags.NonPublic);
             methodInfo.Invoke(solution, null);
+        }
+        object GetSolutionWrapper() {
+            return typeof(BuildManager).Assembly.CreateInstance("Microsoft.Build.Construction.SolutionParser", true, BindingFlags.NonPublic | BindingFlags.Instance, null, new object[] {}, CultureInfo.CurrentCulture, null);
         }
     }
 }
