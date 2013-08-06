4a9c98f3160ba25b6f3594fc21a1e380194c77c1
Xarlot <maximatorr@bk.ru>
2013-08-06 13:58:08 +0400

    add reference

--- src/DXVcsTools.UI/AddReferenceHelper/AddReferenceHelper.cs
+++ src/DXVcsTools.UI/AddReferenceHelper/AddReferenceHelper.cs
@@ -6,19 +6,31 @@ using DXVcsTools.UI.Navigator;
 namespace DXVcsTools.UI {
     public class AddReferenceHelper {
         public void AddReferences(IDteWrapper dte, NavigateItem item) {
-            dte.ClearReferences();
-            SolutionParser parser = new SolutionParser(item.Path);
-            foreach (var assembly in parser.GetReferencedAssemblies(true))
-                dte.AddReference(assembly);
+            try {
+                BusyIndicator.Show();
+                dte.ClearReferences();
+                SolutionParser parser = new SolutionParser(item.Path);
+                foreach (var assembly in parser.GetReferencedAssemblies(true))
+                    dte.AddReference(assembly);
+            }
+            finally {
+                BusyIndicator.Close();
+            }
         }
         public void AddProjectReferences(IDteWrapper dte, NavigateItem item) {
-            dte.ClearReferences();
-            dte.ClearProjectReferences();
-            SolutionParser parser = new SolutionParser(item.Path);
-            foreach (var assembly in parser.GetReferencedAssemblies(false))
-                dte.AddReference(assembly);
-            foreach (var path in parser.GetProjectPathes()) 
-                dte.AddProjectReference(path);
+            try {
+                BusyIndicator.Show();
+                dte.ClearReferences();
+                dte.ClearProjectReferences();
+                SolutionParser parser = new SolutionParser(item.Path);
+                foreach (var assembly in parser.GetReferencedAssemblies(false))
+                    dte.AddReference(assembly);
+                foreach (var path in parser.GetProjectPathes())
+                    dte.AddProjectReference(path);
+            }
+            finally {
+                BusyIndicator.Close();
+            }
         }
         public ProjectType GetProjectType(string path) {
             SolutionParser parser = new SolutionParser(path);
---
+++ src/DXVcsTools.UI/View/BusyIndicator.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Editors.Helpers;
+
+namespace DXVcsTools.UI {
+    public class BusyIndicator {
+        static ProgressBusyIndicator indicator;
+        public static void Show() {
+            indicator.Do(x => x.Close());
+            indicator = new ProgressBusyIndicator();
+            ThemeManager.SetThemeName(indicator, ThemeProvider.Instance.ThemeName);
+            indicator.Show();
+        }
+        public static void Close() {
+            indicator.Do(x => x.Close());
+            indicator = null;
+        }
+    }
+
+    class ProgressBusyIndicator : DXWindow {
+        public ProgressBusyIndicator() {
+            TextBlock tb = new TextBlock() {Text = "Loading", Margin = new Thickness(30)};
+            Content = tb;
+            Topmost = true;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            WindowStyle = WindowStyle.None;
+            SizeToContent = SizeToContent.WidthAndHeight;
+        }
+    }
+}
