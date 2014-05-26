12abc323af39581043acf1d1c4e80fa90c2e96a4
Xarlot <maximatorr@bk.ru>
2014-05-26 19:18:14 +0400

    internal blame

--- src/DXVcsTools.UI/View/InternalBlameControl.xaml.cs
+++ src/DXVcsTools.UI/View/InternalBlameControl.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Mvvm.UI.Interactivity;
 
 namespace DXVcsTools.UI.View {
     /// <summary>
@@ -20,6 +12,23 @@ namespace DXVcsTools.UI.View {
     public partial class InternalBlameControl : UserControl {
         public InternalBlameControl() {
             InitializeComponent();
+        }
+    }
+
+    public class CustomDisplayTextAttachedBehavior : Behavior<GridControl> {
+        protected override void OnAttached() {
+            base.OnAttached();
+            AssociatedObject.CustomColumnDisplayText += AssociatedObject_CustomColumnDisplayText;
+        }
+        protected override void OnDetaching() {
+            base.OnDetaching();
+            AssociatedObject.CustomColumnDisplayText -= AssociatedObject_CustomColumnDisplayText;
+        }
+
+        void AssociatedObject_CustomColumnDisplayText(object sender, CustomColumnDisplayTextEventArgs e) {
+            if (e.Column.FieldName == "Comment") {
+                e.DisplayText = string.IsNullOrEmpty(e.DisplayText) ? e.DisplayText : e.DisplayText.Replace(Environment.NewLine, " ");
+            }
         }
     }
 }
