b8b5522992b2670df046abd88a86d57f7986b274
Xarlot <maximatorr@bk.ru>
2014-05-27 14:05:04 +0400

    internal blame

--- src/DXVcsTools.UI/ViewModel/InternalBlameViewModel.cs
+++ src/DXVcsTools.UI/ViewModel/InternalBlameViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using DevExpress.Mvvm;
 using DXVcsTools.Data;
 
@@ -7,7 +8,16 @@ namespace DXVcsTools.UI.ViewModel {
         IEnumerable<IBlameLine> blame;
         int revision;
         string filePath;
-        int? lineNumber;
+        int lineNumber;
+        IBlameLine currentLine;
+        public string FilePath {
+            get { return filePath; }
+            set { SetProperty(ref filePath, value, () => FilePath); }
+        }
+        public IBlameLine CurrentLine {
+            get { return currentLine; }
+            set { SetProperty(ref currentLine, value, () => CurrentLine); }
+        }
         public int Revision {
             get { return revision; }
             set { SetProperty(ref revision, value, () => Revision); }
@@ -18,10 +28,11 @@ namespace DXVcsTools.UI.ViewModel {
         }
 
         public InternalBlameViewModel(string filePath, int? lineNumber, BlameHelper blameHelper) {
-            this.filePath = filePath;
-            this.lineNumber = lineNumber;
+            this.lineNumber = lineNumber - 1 ?? 0;
+            FilePath = filePath;
             Revision = blameHelper.GetLastRevision(filePath, lineNumber);
             Blame = blameHelper.BlameAtRevision(filePath, lineNumber, Revision);
+            CurrentLine = Blame.ElementAtOrDefault(this.lineNumber);
         }
     }
 }
