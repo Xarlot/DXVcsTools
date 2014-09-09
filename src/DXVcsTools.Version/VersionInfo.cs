eaeea03bd2661675d4f383372503772ef3ec218d
Xarlot <maximatorr@bk.ru>
2014-09-09 12:50:28 +0400

    2.1.24

--- src/DXVcsTools.Version/VersionInfo.cs
+++ src/DXVcsTools.Version/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace DXVcsTools.Version {
     public sealed class VersionInfo {
         public const string Major = "2";
         public const string Minor = "1";
-        public const string Build = "23";
+        public const string Build = "24";
         public const string Revision = "0";
 
         public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision;
