29d8d682298cff946f41ec41167c04f689cb91f5
Xarlot <maximatorr@bk.ru>
2015-08-12 12:50:37 +0300

    1

--- src/DXVcsTools.UI/ViewModel/AnalyticsViewModel.cs
+++ src/DXVcsTools.UI/ViewModel/AnalyticsViewModel.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DXVcsTools.Core;
 
 namespace DXVcsTools.UI.ViewModel {
-    class AnalyticsViewModel {
+    public class AnalyticsViewModel : BindableBaseCore {
     }
 }
